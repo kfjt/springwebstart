83a7f4da994fca52e4d19eedba84cf5b4afafa64
kfjt <kfjt@users.noreply.github.com>
2022-09-23 19:00:14 +0000

    pdf file upload

--- src/main/java/com/example/demo/PdfController.java
+++ src/main/java/com/example/demo/PdfController.java
@@ -1,21 +1,83 @@
 package com.example.demo;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 
 /** pdf. */
 @Controller
+@RequestMapping("pdf")
 public class PdfController {
+  public static final String UPLOAD_DIRECTORY = System.getProperty("user.dir") + "/uploads";
+
+  private List<String> ls(String dir) throws IOException {
+    try (Stream<Path> stream = Files.list(Paths.get(dir))) {
+      return stream.map(Path::getFileName).map(Path::toString).collect(Collectors.toList());
+    }
+  }
 
   /**
-   * GET /pdf.
+   * GET /pdf/ocr.
    *
-   * @return pdf
+   * @return pdf/ocr
    */
-  @GetMapping("/pdf")
-  public String getMapPdf(Model model) {
+  @GetMapping("ocr")
+  public String getOcr() {
+    return "pdf/ocr";
+  }
+
+  /**
+   * POST /pdf/ocr.
+   *
+   * @param model model
+   * @param file file
+   * @return pdf/files
+   * @throws IOException ls
+   */
+  @PostMapping("ocr")
+  public String postOcr(Model model, @RequestParam("pdfFile") MultipartFile file)
+      throws IOException {
+    StringBuilder fileNames = new StringBuilder();
+    fileNames.append(file.getOriginalFilename());
+    Path fileNameAndPath = Paths.get(UPLOAD_DIRECTORY, file.getOriginalFilename());
+    Files.write(fileNameAndPath, file.getBytes());
+
+    model.addAttribute("filelist", ls(UPLOAD_DIRECTORY));
+    return "pdf/files";
+  }
+
+  /**
+   * GET /pdf/files.
+   *
+   * @param model model
+   * @return pdf/files
+   * @throws IOException ls
+   */
+  @GetMapping("files")
+  public String getFiles(Model model) throws IOException {
+    model.addAttribute("filelist", ls(UPLOAD_DIRECTORY));
+    return "pdf/files";
+  }
+
+  /**
+   * GET /pdf/ui.
+   *
+   * @return pdf/ui
+   */
+  @GetMapping("ui")
+  public String getUi(Model model) {
     A4TemplateRoot.Position positionA = new A4TemplateRoot.Position();
     positionA.setX(0);
     positionA.setY(0);
@@ -52,6 +114,6 @@ public class PdfController {
     input.setB("b10\nb20");
     input.setC("c1");
     model.addAttribute("inputs", Arrays.asList(input));
-    return "pdf";
+    return "pdf/ui";
   }
 }
